3fd9683f92b8b096e9b5ebe18967b25f5f99f690
KirikaP <kirikahhh@gmail.com>
2024-08-07 15:51:59 +0100

    delete docs

--- fig3.py
+++ fig3.py
@@ -5,14 +5,6 @@ from kk_multithread import KKNetwork, train, single_update
 
 
 if __name__ == "__main__":
-    """
-    在 Windows 上, multiprocessing 模块通过导入主模块来创建新进程
-    如果模块级代码没有用 if __name__ == "__main__": 保护
-    那么每次创建新进程时都会执行模块的顶层代码
-    1. 这可能导致无限循环，每个进程创建更多进程，最终导致 BrokenProcessPool 或耗尽系统资源
-    2. 这个保护确保了像 ProcessPoolExecutor 这样的代码只在主进程中执行
-       这防止了意外的副作用，并确保每个新进程中仅运行必要的代码部分
-    """
     # Params
     L = 3
     K = 3
