8236e9ea6f636ecb992b3189c2279c11d3735975
KirikaP <kirikahhh@gmail.com>
2024-08-20 00:04:35 +0100

    change instantiation method

--- kk_protocol/scripts/party_machine.py
+++ kk_protocol/scripts/party_machine.py
@@ -4,27 +4,78 @@ from tqdm import tqdm
 
 
 class PM:
+    """
+    A class representing a KK network, which is a simplified model used to study synchronization
+    between networks.
+
+    Attributes:
+    -----------
+    L : int
+        The range limit for the weight values.
+    N : int
+        The number of columns in the weight matrix W.
+    K : int
+        The number of rows in the weight matrix W.
+    zero_replace : int
+        The value to replace 0 with in the output Y (1 for sender, -1 for receiver).
+    W : numpy.ndarray
+        The weight matrix of shape (K, N) with values ranging from -L to L.
+    Y : numpy.ndarray
+        The output vector after applying the update rule.
+    O : int
+        The overall output of the network, which is the product of the elements in Y.
+    """
     def __init__(self, L, N, K, zero_replace):
         self.L, self.N, self.K = L, N, K
         self.W = np.random.choice(np.arange(-L, L + 1), size=(K, N))
         self.zero_replace = zero_replace
 
     def update_O(self, X):
+        """
+        Update the output vector Y and overall output O based on the input matrix X.
+        """
         self.Y = np.sign(np.sum(self.W * X, axis=1))
         self.Y[self.Y == 0] = self.zero_replace  # replace 0 with 1(sender) or -1(receiver)
         self.O = np.prod(self.Y)
 
     def update_W(self, X):
+        """
+        Update the weight matrix W based on the current output and input matrix X.
+        """
         for k in range(self.K):
             if self.O * self.Y[k] > 0:  # if k-th unit is not sync
                 self.W[k] -= self.O * X[k]  # updates W line by line
         self.W = np.clip(self.W, -self.L, self.L)  # clip W to [-L, L]
 
     def is_sync(self, other):
-        # check if self and other are sync
+        """
+        Check if the current network is synchronized with another network.
+
+        Parameters:
+        other : PM
+            Another instance of the PM network to check synchronization with.
+
+        Returns:
+        bool
+            True if the networks are synchronized (W = -other.W), False otherwise.
+        """
         return np.array_equal(self.W, -other.W)
 
     def sync(self, other, stop_on_sync=True):
+        """
+        Synchronize the current network with another network by iteratively updating weights.
+
+        Parameters:
+        other : PM
+            Another instance of the PM network to synchronize with.
+        stop_on_sync : bool, optional
+            If True, stops the synchronization process once the networks are synchronized.
+            Default is True.
+
+        Returns:
+        int
+            The number of steps taken to achieve synchronization.
+        """
         steps = 0
         while True:
             X = np.random.choice([-1, 1], size=(self.K, self.N))
@@ -34,35 +85,34 @@ class PM:
                 self.update_W(X)
                 other.update_W(X)
             steps += 1
-            # Check synchronization condition at the end of each iteration
             if stop_on_sync and self.is_sync(other):
                 break
 
         return steps
 
 
-def train_PMs(L, N, K, num_runs=5000, stop_on_sync=True):
-    # 创建一个进程池执行器，以便并行执行多个任务
+def train_PMs(pm1, pm2, num_runs=5000, stop_on_sync=True):
+    """
+    Train two PM instances by running sync method in parallel.
+
+    Parameters:
+    pm1 (PM): The first PM instance.
+    pm2 (PM): The second PM instance.
+    num_runs (int): Number of parallel runs.
+    stop_on_sync (bool): Whether to stop when the two networks synchronize.
+
+    Returns:
+    list: A list of sync steps for each run.
+    """
     with ProcessPoolExecutor() as executor:
-        # 创建一个包含所有异步任务的列表，任务通过 executor.submit 提交
         futures = [
-            # executor.submit 会提交一个任务到进程池中
-            # 任务的内容是调用 PM(L, N, K, 1) 实例的 sync 方法
-            # sync 方法的第一个参数 self 自动绑定到这个 PM 实例
-            # sync 方法的第二个参数 other_network 则是另一个 PM 实例 PM(L, N, K, -1)
-            # 换句话说，这里同时实例化了两个 PM 对象，并将它们用于同步操作
-            executor.submit(
-                PM(L, N, K, 1).sync,  # 第一个 PM 对象的 sync 方法
-                PM(L, N, K, -1), stop_on_sync  # 作为 sync 方法的参数传递的第二个 PM 对象
-            ) for _ in range(num_runs)
+            executor.submit(pm1.sync, pm2, stop_on_sync)
+            for _ in range(num_runs)
         ]
 
         return [
-            # as_completed 函数会在每个任务完成时生成一个 future 对象
-            # future.result() 会阻塞直到任务完成，并返回任务的结果
             future.result() for future in tqdm(as_completed(futures), total=num_runs)
         ]
-
 
 
 # def train(L, N, K, num_runs=5000):
